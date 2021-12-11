22125f4c5eeb620948502cdc869c8b3c49f458cc
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-11 22:49:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Container = styled.div`
+	width: 1120px;
+	height: 100vh;
+	margin: 0 auto;
+	background: red;
+`
+
+const App = () => {
+	
+	const [data, setData] = useState([])
+
+	useEffect(()=>{
+		fetch(`http://go-dev.greedygame.com/v3/dummy/report?startDate=2021-05-01&endDate=2021-05-03`)
+			.then(response => response.json())
+				.then(result => setData(result.data))
+				.catch(err => console.log('API fetching error => ', err))
+	}, [])
+
+	console.log(data)
+	return (
+		<Container>
+			<table>
+				<tr>
+					<th>Date</th>
+					<th>App</th>
+					<th>Request</th>
+					<th>Response</th>
+					<th>Impression</th>
+					<th>Clicks</th>
+					<th>Revenue</th>
+					<th>Fill Rate</th>
+					<th>CTR</th>
+
+				</tr>
+				{data && data.map((item, index)=> 
+					<tr key={item.app_id}>
+						<td>{item.date}</td>
+						<td>{item.date}</td>
+						<td>{item.date}</td>
+					</tr>
+				)}
+			</table>
+		</Container>
+	);
 }
-
+ 
 export default App;
