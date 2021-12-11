4c8dac05c8b4bb885437f2699b3f821dcfaad7ab
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-12 00:35:37 +0530

    top columns, styling and font style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import Table from './components/table';
 
 const Container = styled.div`
 	width: 1120px;
 	height: 100vh;
 	margin: 0 auto;
-	background: red;
+	background: #fff;
+	padding: 30px;
 `
+
+const MainCta = styled.div`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-top: 20px;
+  margin-bottom: 10px;
+	.main-cta-buttons{
+    border: 1px solid #959595;
+    padding: 5px;
+    border-radius: 4px;
+    font-size: 14px;
+	}
+`
+
+const TableColumnList = styled.div`
+	border: 1px solid #959595;
+	padding: 20px;
+	border-radius: 10px;
+	.col-list-title{
+    font-size: 14px;
+	}
+	.list-grid {
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+		grid-gap: 10px;
+		margin-top: 10px;
+		.pills {
+			border: 1px solid #959595;
+			padding: 5px;
+			border-radius: 4px;
+			font-size: 14px;
+			font-family: 'Open Sans', sans-serif;
+		}
+	}
+	.ctas{
+		text-align: right;
+		margin-top: 20px;
+		button{
+			border: none;
+			font-size: 14px;
+			border-radius: 2px;
+			padding: 10px 15px;
+		}
+		.secondary{
+			color: #136fed;
+			border: 1px solid #959595;
+			background: none;
+			margin-right: 20px;
+		}
+		.primary{
+			color: #fff;
+			background: #136fed;
+		}
+	}
+`
+
+const colListData = [
+	"Date",
+	"App",
+	"Clicks",
+	"Ad Requests",
+	"Ad Response",
+	"Impression",
+	"Revenue",
+	"Fill Rate",
+	"CTR"
+]
 
 const App = () => {
 	
@@ -22,27 +92,40 @@ const App = () => {
 	console.log(data)
 	return (
 		<Container>
-			<table>
-				<tr>
-					<th>Date</th>
-					<th>App</th>
-					<th>Request</th>
-					<th>Response</th>
-					<th>Impression</th>
-					<th>Clicks</th>
-					<th>Revenue</th>
-					<th>Fill Rate</th>
-					<th>CTR</th>
+			<p className="title">Analytics</p>
 
-				</tr>
-				{data && data.map((item, index)=> 
-					<tr key={item.app_id}>
-						<td>{item.date}</td>
-						<td>{item.date}</td>
-						<td>{item.date}</td>
-					</tr>
-				)}
-			</table>
+			<MainCta className="main-cta">
+				<div className="main-cta-buttons">
+					July 08 - July 14, 2021
+				</div>
+
+				<div className="main-cta-buttons">
+					Settings
+				</div>
+			</MainCta>
+
+			<TableColumnList className="table-column-list">
+				<p className="col-list-title">Dimensions and Metrics</p>
+				<div className="list-grid">
+					{colListData.map((item, index)=>
+						<div key={index} className="pills">
+							{item}
+						</div>
+					)}
+				</div>
+
+				<div className="ctas">
+					<button className="secondary">
+						Close
+					</button>
+					<button className="primary">
+						Apply Changes
+					</button>
+				</div>
+			</TableColumnList>
+
+
+			<Table />
 		</Container>
 	);
 }
---
+++ src/components/table/table.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Table = () => {
+	return (
+		<>
+			<p>I am table</p>
+			{/* TABLE STARTS HERE */}
+			
+		</>
+	);
+}
+ 
+export default Table;
