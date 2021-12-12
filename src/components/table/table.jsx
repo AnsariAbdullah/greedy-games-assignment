6b8c8101f854a34d34e7fa6c1dc1cefed4ee971b
AnsariAbdullah <ansariabdullahar@gmail.com>
2021-12-12 12:41:35 +0530

    make tables

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,11 +1,134 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TableContainer = styled.div`
+	width: 100%;
+	margin-top: 20px;
+	table{
+		width: 100%;
+		thead{
+			tr{
+				th{
+					text-align: left;
+				}
+			}
+		}
+	}
+`
+
+const data = [
+	{
+		date: "14 July 2021",
+		app_name: "ShareChat", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "Callbreak Menu", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "ShareChat", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "Callbreak Menu", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "ShareChat", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "Callbreak Menu", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "ShareChat", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+	{
+		date: "14 July 2021",
+		app_name: "Callbreak Menu", 
+		clicks: 1003200,
+		requests: 1004212,
+		app_id: "123456",
+		impressions: 1003934,
+		responses: 1004162,
+		revenue: 130.98563305583622,
+	},
+]
 
 const Table = () => {
 	return (
 		<>
-			<p>I am table</p>
 			{/* TABLE STARTS HERE */}
-			
+			<TableContainer className="table-container">
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>App</th>
+							<th>Clicks</th>
+							<th>Request</th>
+							<th>Revenue</th>
+							<th>Impression</th>
+						</tr>
+					</thead>
+					<tbody>
+						{data && data.map((item, index)=> 
+							<tr key={index}>
+								<td className="date">{item.date}</td>
+								<td className="app">{item.app_name}</td>
+								<td className="clicks">{item.clicks}</td>
+								<td className="request">{item.requests}</td>
+								<td className="revenue">${item.revenue}</td>
+								<td className="impression">{item.impressions}</td>
+							</tr>
+						)}
+					</tbody>
+				</table>
+			</TableContainer>
 		</>
 	);
 }
